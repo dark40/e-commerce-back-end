7714d41c6b6d0f080c49290b4a3ec6aee597508b
Freddie Yang <dark_yty@hotmail.com>
2022-08-21 01:10:33 +1000

    fixed bugs on put method and delete method

--- main/routes/api/product-routes.js
+++ main/routes/api/product-routes.js
@@ -28,7 +28,10 @@ router.get('/:id', async (req, res) => {
 
     if (!productData) {
       res.status(404).json({message: 'No product found with this id!'})
+      return;
     }
+    
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
