06a1ce8699d3d45bcff8ebeaef00f9f362e9e615
Freddie Yang <dark_yty@hotmail.com>
2022-08-20 23:06:46 +1000

    fixed the async functions

--- main/models/index.js
+++ main/models/index.js
@@ -23,12 +23,12 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product), {
+Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
   }
-};
+});
 
 module.exports = {
   Product,
