ba36740e79ce941a5575886c751f3a8323e8670b
Freddie Yang <dark_yty@hotmail.com>
2022-08-21 11:01:43 +1000

    updated format

--- main/routes/api/category-routes.js
+++ main/routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      include: [{ model: Product }]
     })
     res.status(200).json(categoryData);
   } catch (err) {
@@ -21,11 +21,11 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product}]
+      include: [{ model: Product }]
     })
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' })
-      return; 
+      return;
     }
 
     res.status(200).json(categoryData);
